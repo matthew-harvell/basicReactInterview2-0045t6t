7656f84649e33369bfda561dc3076da0b0777ca8
Matthew G. Harvell <mharvell@ehs.com>
2022-07-07 09:38:52 -0500

    Ready for interview

---
+++ src/stringUtils.js
@@ -0,0 +1,16 @@
+class StringUtils {
+  manipulateString = (input) => {
+    /*
+        Given a string in the English alphabet utilizing only upper case and lower case letters (i.e., A-Z and a-z only),
+        create a new string which consists of the opposite case (lower to upper and vice versa)
+        which is also reversed in character order from the original.  E.g., "ABCD" would become "dcba". If the character is NOT
+        a letter character, remove it from the result.   For reference, the ASCII and Unicode values of the upper case 
+        number are 65 - 90 and the lower case letters are 32 above the corresponding upper case. Try to keep in mind that all
+        primitive values (such as string, number, boolean...) are immutable in JavaScript - try to keep
+        the amount of memory allocations as low as possible.  
+    */
+    throw new Error('Method Not Yet Implemented.')
+  };
+}
+
+module.exports = StringUtils;
